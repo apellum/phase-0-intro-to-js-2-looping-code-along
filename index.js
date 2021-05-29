9a6e21fe26aa95a64666aa77e6757b338c48867a
Andrew Pellum <apellum19@gmail.com>
2021-05-28 19:14:11 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+function writeCards(names) {
+    let newNames = [];
+    for (let i = 0; i < names.length; i++)[
+    newNames.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    ]
+    return newNames;
+}
+function countDown(number) {
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log(countDown--);
+}
+}
